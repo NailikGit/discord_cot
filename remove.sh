0914972c020b7e669e2bd4d8777e150a0eb2266b
NailikGit <kilian.smeets@web.de>
2025-01-01 20:11:50 +0100

    remove echo from bash scripts & add guild id file

--- remove.sh
+++ remove.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 token=$(<../token)
-echo $token
 
 sed -i -e "s/$token/YOUR_BOT_TOKEN/g" $1
--- replace.sh
+++ replace.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 token=$(<../token)
-echo $token
 
 sed -i -e "s/YOUR_BOT_TOKEN/$token/g" $1
