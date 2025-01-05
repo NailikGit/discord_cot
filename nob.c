7c0a18d0267930f4f4c5b06f4050f2d51e21389b
NailikGit <kilian.smeets@web.de>
2025-01-05 22:45:56 +0100

    rename ping_pong to nutte to reflect application name

--- nob.c
+++ nob.c
@@ -5,12 +5,11 @@ int main(int argc, char** argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
   nob_cmd_append(&cmd, "clang");
-  nob_cmd_append(&cmd, "src/ping_pong.c", "-o", "build/ping_pong");
+  nob_cmd_append(&cmd, "src/nutte.c", "-o", "build/nutte");
   nob_cmd_append(&cmd, "-g", "-Wall", "-Wextra", "-DCCORD_SIGINTCATCH");
   nob_cmd_append(&cmd, "-pthread", "-ldiscord", "-lcurl");
-  if(!nob_cmd_run_sync(cmd)) return 1;
+  if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
 
-  cmd.count = 0;
   nob_cmd_append(&cmd, "build/ping_pong");
   if(!nob_cmd_run_sync(cmd)) return 1;
 
