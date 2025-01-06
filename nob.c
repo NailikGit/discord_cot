4886fe464724c21a9739bfc4c61340172c81a656
NailikGit <kilian.smeets@web.de>
2025-01-06 05:28:34 +0100

    change use nob internal function for mkdir

--- nob.c
+++ nob.c
@@ -5,8 +5,7 @@ int main(int argc, char** argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
 
-  nob_cmd_append(&cmd, "mkdir", "-p", "build");
-  nob_cmd_run_sync_and_reset(&cmd);
+  if(!nob_mkdir_if_not_exists("build")) return 1;
   
   nob_shift_args(&argc, &argv);
   if(argc == 0) {
