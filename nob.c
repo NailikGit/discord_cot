0bc66557b3823afb44dff845349d52b05228c106
NailikGit <kilian.smeets@web.de>
2025-01-08 17:10:05 +0100

    add chain commands

--- nob.c
+++ nob.c
@@ -14,21 +14,25 @@ int main(int argc, char** argv) {
     return 0;
   }
 
-  if(strcmp(argv[0], "build") == 0) {
-    nob_cmd_append(&cmd, "clang");
-    nob_cmd_append(&cmd, "src/nutte.c", "-o", "build/nutte");
-    nob_cmd_append(&cmd, "-g", "-Wall", "-Wextra", "-DCCORD_SIGINTCATCH");
-    nob_cmd_append(&cmd, "-pthread", "-ldiscord", "-lcurl");
-    nob_log(NOB_INFO, "building nutte...");
-    if(!nob_cmd_run_sync(cmd)) return 1;
-  } else if(strcmp(argv[0], "run") == 0) {
-    nob_cmd_append(&cmd, "build/nutte");
-    nob_log(NOB_INFO, "starting nutte...");
-    if(!nob_cmd_run_sync(cmd)) return 1;
-  } else {
-    nob_log(NOB_ERROR, "invalid command '%s'", argv[0]);
-    nob_log(NOB_INFO, "usage: ./nob <build|run>");
-  }
+  while(argc) {
+    if(strcmp(argv[0], "build") == 0) {
+      nob_cmd_append(&cmd, "clang");
+      nob_cmd_append(&cmd, "src/nutte.c", "-o", "build/nutte");
+      nob_cmd_append(&cmd, "-g", "-Wall", "-Wextra", "-DCCORD_SIGINTCATCH");
+      nob_cmd_append(&cmd, "-pthread", "-ldiscord", "-lcurl");
+      nob_log(NOB_INFO, "building nutte...");
+      if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    } else if(strcmp(argv[0], "run") == 0) {
+      nob_cmd_append(&cmd, "build/nutte");
+      nob_log(NOB_INFO, "starting nutte...");
+      if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    } else {
+      nob_log(NOB_ERROR, "invalid command '%s'", argv[0]);
+      nob_log(NOB_INFO, "usage: ./nob <build|run>");
+      return 1;
+    }
 
+    nob_shift_args(&argc, &argv);
+  }
   return 0;
 }
